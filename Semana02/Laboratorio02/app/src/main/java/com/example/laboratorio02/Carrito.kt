b3c1bda1366474166f46d422335d8bb9f940f36c
AntonioPromaweb <vasquezfloresluisantonio33@gmail.com>
2026-08-27 23:47:59 -0500

    POO 3: Agrega insercion de productos y reporte alineado en CarritoDeCompras

--- Semana02/Laboratorio02/app/src/main/java/com/example/laboratorio02/Carrito.kt
+++ Semana02/Laboratorio02/app/src/main/java/com/example/laboratorio02/Carrito.kt
@@ -54,4 +54,19 @@ fun main() {
     println("TIENDA TECSUP")
     println("==")
     println("Cliente: $cliente")
+
+    carrito.agregarProducto(ProductoFisico("Laptop HP", 2500.0, 1))
+    carrito.agregarProducto(ProductoFisico("Mouse Logitech", 45.5, 2))
+    carrito.agregarProducto(ProductoFisico("Audifonos Sony", 120.0, 1))
+    carrito.agregarProducto(ProductoFisico("USB Kingston 64GB", 25.0, 3))
+
+    println("DETALLE DEL CARRITO")
+    var idx = 1
+    for (p in carrito.obtenerProductos()) {
+        if (p is ProductoFisico) {
+            val sub = p.calcularSubtotal()
+            println(String.format("%d. %-20s x%d  S/%8.2f", idx, p.getNombre(), p.getCantidad(), sub))
+            idx++
+        }
+    }
 }
