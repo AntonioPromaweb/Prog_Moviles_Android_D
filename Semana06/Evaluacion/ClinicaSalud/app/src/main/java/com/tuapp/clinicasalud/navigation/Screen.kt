be57e46d785820c955256727b12330ef702c883e
AntonioPromaweb <vasquezfloresluisantonio33@gmail.com>
2026-09-23 10:32:34 -0500

    Configuración inicial del proyecto ClinicaSalud con navegación y rutas definidas

---
+++ Semana06/Evaluacion/ClinicaSalud/app/src/main/java/com/tuapp/clinicasalud/navigation/Screen.kt
@@ -0,0 +1,22 @@
+package com.tuapp.clinicasalud.navigation
+
+sealed class Screen(val route: String) {
+    object Home : Screen(route = "home")
+
+    object DoctorProfile : Screen(route = "doctorProfile/{doctorId}") {
+        fun createRoute(doctorId: Int): String = "doctorProfile/$doctorId"
+    }
+
+    object Schedule : Screen(route = "schedule/{doctorId}") {
+        fun createRoute(doctorId: Int): String = "schedule/$doctorId"
+    }
+
+    object Confirmation : Screen(route = "confirmation/{doctorId}/{date}/{time}") {
+        fun createRoute(doctorId: Int, date: String, time: String): String =
+            "confirmation/$doctorId/$date/$time"
+    }
+
+    object MyAppointments : Screen(route = "myAppointments")
+    object MedicalHistory : Screen(route = "medicalHistory")
+}
+}
